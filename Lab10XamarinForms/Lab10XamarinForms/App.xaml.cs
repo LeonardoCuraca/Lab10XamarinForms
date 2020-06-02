8dfacfa2af69834926e0ce595220a011935401ab
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-06-01 19:25:40 -0500

    first commit

---
+++ Lab10XamarinForms/Lab10XamarinForms/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Lab10XamarinForms.Services;
+using Lab10XamarinForms.Views;
+
+namespace Lab10XamarinForms
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Lab10XamarinForms/Lab10XamarinForms/Models/HomeMenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10XamarinForms.Models
+{
+    public enum MenuItemType
+    {
+        Browse,
+        About,
+        ViewToViewDemo,
+        BindingModeDemo,
+        ListViewDemo,
+        PickerDemo
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
